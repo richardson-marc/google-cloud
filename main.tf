8b4755f9c6f82222710f0f1380339e1a9e4fc15b
Marc Richardson <marcr@blarg.net>
2019-03-26 10:57:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("OVPs-2533a1e2e3a5.json")}"
+ credentials = "${file("some_credentials.json")}"
  project     = "ovps-232122"
  region      = "us-west1"
 }
